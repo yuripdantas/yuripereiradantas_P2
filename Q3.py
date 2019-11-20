4d1ce1bccfa4df43ac8944beaf98caab9b4245eb
Yuri <yuripdantas@poli.ufrj.br>
2019-11-20 16:42:08 -0300

    Nao consegui fazer essa dentro do prazo

--- Q3.py
+++ Q3.py
@@ -1,3 +1,10 @@
 #Nome: Yuri Pereira Dantas
 #Questao 3 da P2 de Comp1 / Professor Juliano
-#versao: do jeito exato que escrevi na minha prova
+#versao: corrigida
+
+# Caro Professor,
+#
+#Não consegui dentro das 24h fazer essa questao 3, devido precisar estudar para o restante
+#das provas dessa semana. Pretendo faze-la com calma depois, mas ja vou estar fora do prazo. 
+#Um Abraço
+#Yuri Pereira Dantas
